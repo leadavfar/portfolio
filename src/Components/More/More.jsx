5fef3a34da988960ca05ef2a47abdbe71d105cb7
Leandro Farias <leandrodavidfarias@gmail.com>
2022-08-25 04:32:58 -0300

    done

--- src/Components/More/More.jsx
+++ src/Components/More/More.jsx
@@ -3,6 +3,7 @@ export default function More() {
     return (
         <div>
             <h1>CHOREO PAPÁ</h1>
+            <h2>Hay que seguir choreando por culpa de branch DEV</h2>
         </div>
     )
 }
