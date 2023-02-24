f60e4678a226d94e2cb9929ac2d37a7e2173c94c
Leandro Farias <leandrodavidfarias@gmail.com>
2023-02-24 04:41:25 -0300

    new cv

--- src/Components/Technologies/Technologies.jsx
+++ src/Components/Technologies/Technologies.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-    SiJavascript, SiNodedotjs, SiHtml5, SiCss3, SiTypescript, SiReact, SiRedux, SiMaterialui, SiExpress,
+    SiJavascript, SiNodedotjs, SiHtml5, SiCss3, SiTypescript, SiReact, SiAngular, SiRedux, SiMaterialui, SiExpress,
     SiPostgresql, SiSequelize, SiMongodb, SiGit,
 } from "react-icons/si";
 import { IconContext } from "react-icons";
@@ -16,12 +16,13 @@ export default function Technologies() {
                     <div className={Styles.iconOrder}><SiJavascript />JavaScript</div>
                     <div className={Styles.iconOrder}><SiTypescript />TypeScript</div>
                     <div className={Styles.iconOrder}><SiNodedotjs />NodeJs</div>
+                    <div className={Styles.iconOrder}><SiExpress />Express</div>
                     <div className={Styles.iconOrder}><SiHtml5 />HTML5</div>
                     <div className={Styles.iconOrder}><SiCss3 />CSS3</div>
                     <div className={Styles.iconOrder}><SiReact />React</div>
                     <div className={Styles.iconOrder}><SiRedux />Redux</div>
-                    {/* <div className={Styles.iconOrder}><SiMaterialui />Material UI</div> */}
-                    <div className={Styles.iconOrder}><SiExpress />Express</div>
+                    <div className={Styles.iconOrder}><SiAngular />Angular</div>
+                    <div className={Styles.iconOrder}><SiMaterialui />Material UI</div>
                     <div className={Styles.iconOrder}><SiPostgresql />PostgreSQL</div>
                     <div className={Styles.iconOrder}><SiSequelize />Sequelize</div>
                     <div className={Styles.iconOrder}><SiMongodb />MongoDB</div>
